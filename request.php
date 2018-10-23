9d5ca853d9b42c9cb5ed32fd116900b25d375e6f
Fakhri Fauzan <fazan697@gmail.com>
2018-10-23 09:22:09 +0700

    add basic sending request

---
+++ request.php
@@ -0,0 +1,31 @@
+<?php
+  require 'vendor/autoload.php';
+
+  use GuzzleHttp\Client;
+
+  $client = new Client(['base_uri' => 'https://jsonplaceholder.typicode.com/']);
+
+  $response = $client->get(
+    'posts/1'
+  );
+  var_dump($response);
+  echo $response->getBody();
+
+  $response = $client->get(
+    'posts/2'
+  );
+  var_dump($response);
+  echo $response->getBody();
+
+  $response = $client->get(
+    'comments/1'
+  );
+  var_dump($response);
+  echo $response->getBody();
+
+  $response = $client->get(
+    'http://httpbin.org/ip'
+  );
+  var_dump($response);
+  echo $response->getBody();
+?>
